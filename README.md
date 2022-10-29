57a1e0fe13855235dcfd51f9d6e66c7534c7505b
vinson <wincentlauvinson@gmail.com>
2022-10-29 12:09:42 +0800

    init filebeat-ck

---
+++ README.md
@@ -0,0 +1,58 @@
+# filebeat-ck
+
+本项目为filebeat输出插件，支持将事件推送到clickhouse数据表，您需要在filebeat中引入本项目并且重新编译filebeat。
+
+## 一、安装编译
+
+### 1、下载beats
+
+```shell
+git clone git@github.com:elastic/beats.git
+```
+
+### 2、更改beats outputs includes文件, 添加filebeat-ck
+
+includes所在目录：beats/libbeat/publisher/includes/includes.go  
+添加如下代码：
+
+```go
+import (
+...
+_ "github.com:lauvinson/filebeat-ck"
+)
+```
+
+### 3、构建filebeat
+
+filebeat所在目录：beats/filebeat  
+执行如下命令：
+
+```shell
+make
+```
+
+## 二、配置
+
+### filebeat-ck插件配置
+
+```yml
+#----------------------------- ClickHouse output --------------------------------
+output.clickHouse:
+  # clickhouse数据库配置
+  # https://github.com/ClickHouse/clickhouse-go
+  # 示例 tcp://host1:9000?username=user&password=qwerty&database=clicks&read_timeout=10&write_timeout=20&alt_hosts=host2:9000,host3:9000
+  url: "tcp://127.0.0.1:9000?debug=true&read_timeout=10&write_timeout=20"
+  # 接收数据的表名
+  table: ck_test
+  # 数据过滤器的表列，匹配日志文件中对应的键
+  columns: [ "id", "name", "created_date" ]
+  # 异常重试休眠时间 单位：秒
+  retry_interval: 60
+  # 是否跳过异常事件推送 true-表示跳过执行异常实践 false-会一直重试，重试间隔为retry_interval
+  skip_unexpected_type_row: false
+  # 批处理数据量，影响filebeat每次抓取数据行
+  bulk_max_size: 1000
+  # 如果配置了grok，可以使用grok的结果作为数据过滤器的表列
+  grok: "remote_addr\[(?:%{NOTSPACE:remote_addr}?|%{DATA})\] \| time_local\[(?:%{HTTPDATE:time_local}?|%{DATA})\] \| server_name\[(?:%{DATA:server_name}?|%{DATA})\] \| request\[(?:%{WORD:verb} %{GREEDYDATA:path}\?%{GREEDYDATA:uri_query} HTTP/%{NUMBER:http_version}?|%{DATA})\] \| status\[(?:%{NUMBER:status}?|%{DATA})\] \| body_bytes_sent\[(?:%{NUMBER:body_bytes_sent}?|%{DATA})\] \| http_referer\[(?:%{NOTSPACE:http_referer}?|%{DATA})\] \| http_user_agent\[(?:%{QUOTEDSTRING:http_user_agent}?|%{DATA})\] \| http_x_forwarded_for\[(?:%{NOTSPACE:http_x_forwarded_for}?|%{DATA})\]"
+  grok_field: "message"
+```
---
+++ client.go
@@ -0,0 +1,270 @@
+package clickhouse_20200328
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+	"github.com/elastic/beats/v7/libbeat/beat"
+	"github.com/elastic/beats/v7/libbeat/logp"
+	"github.com/elastic/beats/v7/libbeat/outputs"
+	"github.com/elastic/beats/v7/libbeat/publisher"
+	"strings"
+	"sync"
+	"time"
+
+	_ "github.com/ClickHouse/clickhouse-go"
+	"github.com/vjeantet/grok"
+)
+
+type client struct {
+	log                   *logp.Logger
+	observer              outputs.Observer
+	url                   string
+	table                 string
+	columns               []string
+	grokStr               string
+	grokField             string
+	retryInterval         int
+	connect               *sql.DB
+	mutex                 sync.Mutex
+	skipUnexpectedTypeRow bool
+	g                     *grok.Grok
+}
+
+func newClient(
+	observer outputs.Observer,
+	url string,
+	table string,
+	columns []string,
+	grokStr string,
+	grokField string,
+	retryInterval int,
+	skipUnexpectedTypeRow bool,
+) *client {
+	client := &client{
+		log:                   logp.NewLogger("clickhouse"),
+		observer:              observer,
+		url:                   url,
+		table:                 table,
+		columns:               columns,
+		grokStr:               grokStr,
+		grokField:             grokField,
+		retryInterval:         retryInterval,
+		skipUnexpectedTypeRow: skipUnexpectedTypeRow,
+	}
+	if grokStr != "" && grokField != "" {
+		g, err := grok.NewWithConfig(&grok.Config{NamedCapturesOnly: true})
+		if err != nil {
+			client.log.Errorf("grok compile error: {%+v}", err)
+			return nil
+		}
+		client.g = g
+	}
+	return client
+}
+
+func (c *client) Connect() error {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	if c.connect != nil {
+		c.log.Infof("connection reuse")
+		return nil
+	}
+	connect, err := sql.Open("clickhouse", c.url)
+	if err != nil {
+		c.connect = nil
+		c.log.Errorf("open clickhouse connection fail: {%+v}", err)
+		return err
+	}
+	if err = connect.Ping(); err != nil {
+		c.connect = nil
+		c.log.Errorf("ping clickhouse fail: {%+v}", err)
+		return err
+	}
+	c.log.Infof("new connection")
+
+	c.connect = connect
+	return err
+}
+
+func (c *client) Close() error {
+	c.log.Infof("close connection")
+	return c.connect.Close()
+}
+
+func (c *client) Publish(_ context.Context, batch publisher.Batch) error {
+	if c == nil {
+		panic("no client")
+	}
+	if batch == nil {
+		panic("no batch")
+	}
+
+	events := batch.Events()
+	c.observer.NewBatch(len(events))
+	rest, err := c.publish(events)
+	if rest != nil {
+		c.observer.Failed(len(rest))
+		c.sleepBeforeRetry(err)
+		batch.RetryEvents(rest)
+		return err
+	}
+
+	batch.ACK()
+	return err
+}
+
+func (c *client) String() string {
+	return "clickhouse(" + c.url + ")"
+}
+
+// publish events
+func (c *client) publish(data []publisher.Event) ([]publisher.Event, error) {
+	ctx := context.Background()
+	formatRows := make([][]interface{}, 0)
+	// group events
+	okFormatEvents, failFormatEvents, formatRows := extractDataFromEvent(c.log, formatRows, data, c.columns, c.grokStr, c.grokField, c.g)
+	c.log.Infof("[check data format] ok-format-events: %d, fail-format-events: %d, format-rows: %d", len(okFormatEvents), len(failFormatEvents), len(formatRows))
+
+	if len(okFormatEvents) == 0 {
+		return failFormatEvents, errors.New("[check data format] all events match field fail")
+	}
+	tx, err := c.connect.BeginTx(ctx, nil)
+	if err != nil {
+		c.log.Errorf("[transaction] begin fail: {%+v}", err)
+		return data, err
+	}
+	stmt, err := tx.PrepareContext(ctx, generateSql(c.table, c.columns))
+	if err != nil {
+		c.log.Errorf("[transaction] stmt prepare fail: {%+v}", err)
+		return data, err
+	}
+	// defer
+	defer stmt.Close()
+	var lastErr error
+	var okExecEvents, failExecEvents []publisher.Event
+	for k, row := range formatRows {
+		_, err = stmt.ExecContext(ctx, row...)
+		if err != nil {
+			c.log.Errorf("[transaction] stmt exec fail: {%+v}", err)
+
+			lastErr = err
+			// fail
+			failExecEvents = append(failExecEvents, okFormatEvents[k])
+			continue
+		}
+		// ok
+		okExecEvents = append(okExecEvents, okFormatEvents[k])
+	}
+	c.log.Infof("[check data type] ok-exec-events: %d, fail-exec-events: %d", len(okExecEvents), len(failExecEvents))
+	// happen error, skip unexpected type row
+	if lastErr != nil && c.skipUnexpectedTypeRow {
+		// rollback
+		tx.Rollback()
+		if len(okExecEvents) > 0 {
+			c.log.Infof("[skip unexpected type row] recall publish, ok-exec-events: %d, fail-exec-events: %d", len(okExecEvents), len(failExecEvents))
+			c.publish(okExecEvents)
+		}
+		return nil, nil
+	}
+	if err = tx.Commit(); err != nil {
+		tx.Rollback()
+		c.log.Errorf("[transaction] commit failed, ok-exec-events: %d, fail-exec-events: %d, err: {%+v}", len(okExecEvents), len(failExecEvents), err)
+		return data, err
+	}
+
+	c.log.Infof("[transaction] commit successed, ok-exec-events: %d, fail-exec-events: %d", len(okExecEvents), len(failExecEvents))
+	return failExecEvents, lastErr
+}
+
+// sleepBeforeRetry sleep before retry
+func (c *client) sleepBeforeRetry(err error) {
+	c.log.Errorf("will sleep for %v seconds because an error occurs: %s", c.retryInterval, err)
+	time.Sleep(time.Second * time.Duration(c.retryInterval))
+}
+
+// generateSql
+func generateSql(table string, columns []string) string {
+	size := len(columns) - 1
+	var columnStr, valueStr strings.Builder
+	for i, cl := range columns {
+		columnStr.WriteString(cl)
+		valueStr.WriteString("?")
+		if i < size {
+			columnStr.WriteString(",")
+			valueStr.WriteString(",")
+		}
+	}
+
+	return fmt.Sprint("insert into ", table, " (", columnStr.String(), ") values (", valueStr.String(), ")")
+}
+
+// extractDataFromEvent extract data
+func extractDataFromEvent(
+	log *logp.Logger,
+	to [][]interface{},
+	data []publisher.Event,
+	columns []string,
+	grokStr string,
+	grokField string,
+	g *grok.Grok,
+) ([]publisher.Event, []publisher.Event, [][]interface{}) {
+	var okEvents, failEvents []publisher.Event
+	for _, event := range data {
+		content := event.Content
+		var row []interface{}
+		var err error
+		if grokField != "" && grokStr != "" {
+			row, err = matchFieldsGrok(content, columns, grokStr, grokField, g)
+		} else {
+			row, err = matchFields(content, columns)
+		}
+		if err != nil {
+			log.Errorf("match field error: {%+v}", err)
+			// match fail then append fail-events
+			failEvents = append(failEvents, event)
+			continue
+		}
+		to = append(to, row)
+		// match successed then append ok-events
+		okEvents = append(okEvents, event)
+	}
+	return okEvents, failEvents, to
+}
+
+// matchFields match field format
+func matchFields(content beat.Event, columns []string) ([]interface{}, error) {
+	row := make([]interface{}, 0)
+	for _, col := range columns {
+		if _, ok := content.Fields[col]; !ok {
+			return nil, errors.New("format error")
+		}
+		val, err := content.GetValue(col)
+		if err != nil {
+			return nil, err
+		}
+		// strict mode
+		//if val == nil {
+		//	return nil, errors.New("row field is empty")
+		//}
+		row = append(row, val)
+	}
+	return row, nil
+}
+
+// matchFields match field format with grok
+func matchFieldsGrok(content beat.Event, columns []string, grokStr, grokField string, g *grok.Grok) ([]interface{}, error) {
+	row := make([]interface{}, 0)
+	values, err := g.Parse(grokStr, content.Fields[grokField].(string))
+	if err != nil {
+		return nil, errors.New("grok cannot parse event")
+	}
+	for _, col := range columns {
+		if _, ok := values[col]; !ok {
+			return nil, errors.New("format error")
+		}
+		row = append(row, values[col])
+	}
+	return row, nil
+}
