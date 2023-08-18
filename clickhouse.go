d6d616a0846d232f102a6f336b194858d1f7cbda
vinson <wincentlauvinson@gmail.com>
2023-08-18 14:23:04 +0800

    feat(ck)

--- clickhouse.go
+++ clickhouse.go
@@ -1,6 +1,7 @@
 package clickhouse_20200328
 
 import (
+	"errors"
 	"github.com/elastic/beats/v7/libbeat/logp"
 	"github.com/elastic/beats/v7/libbeat/outputs"
 )
@@ -9,4 +10,29 @@ var logger = logp.NewLogger("ClickHouse")
 
 func init() {
 	outputs.RegisterType("clickHouse", makeClickHouse)
+}
+
+func makeClickHouse(
+	_ outputs.IndexManager,
+	beat beat.Info,
+	observer outputs.Observer,
+	cfg *common.Config,
+) (outputs.Group, error) {
+
+	config := defaultConfig
+	if err := cfg.Unpack(&config); err != nil {
+		return outputs.Fail(err)
+	}
+
+	if len(config.Table) == 0 {
+		return outputs.Fail(errors.New("ClickHouse: the table name must be set"))
+	}
+
+	if len(config.Columns) == 0 {
+		return outputs.Fail(errors.New("ClickHouse: the table columns must be set"))
+	}
+
+	client := newClient(observer, config.Url, config.Table, config.Columns, config.Grok, config.GrokField, config.RetryInterval, config.SkipUnexpectedTypeRow)
+
+	return outputs.Success(config.BulkMaxSize, config.MaxRetries, client)
 }
